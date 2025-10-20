ce4f438b12223e3a03aba19abdae2f2894440ccf
Jean Kassio <jeankassiocheib@gmail.com>
2025-10-20 01:59:53 -0300

    Bugfix: cannot read properties of undefined

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -2256,7 +2256,7 @@ export class WAStartupService {
       let mediaType: string;
 
       for (const type of TypeMediaMessage) {
-        mediaMessage = msg.message[type];
+        mediaMessage = msg?.message?.[type];
         if (mediaMessage) {
           mediaType = type;
           break;
